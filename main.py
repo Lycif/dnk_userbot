0ac51fe9536c336c93f82f463f69a5a177ad62b3
Lycif <zablotskiyalex001@gmail.com>
2025-07-20 00:37:41 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import os
 
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
 session_string = os.environ["SESSION_STRING"]
+bot_username = "genereytposters_bot"
+
+keywords = ["шахеды", "БПЛА", "Ракети", "балістика", "Бій", "Усік", "Новини"]
+
+channels = [
+    "https://t.me/RLSukr",
+    "https://t.me/+P8imXZKT0VIzMzRi",
+    "https://t.me/noviyykanal"
+]
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-async def main():
-    me = await client.get_me()
-    print(f"Logged in as {me.first_name} ({me.id})")
+def contains_keywords(text):
+    return any(word.lower() in text.lower() for word in keywords)
+
+@client.on(events.NewMessage(chats=channels))
+async def handler(event):
+    message = event.message
+    if message.text and contains_keywords(message.text):
+        await client.send_message(bot_username, message.text)
 
 with client:
-    client.loop.run_until_complete(main())
+    client.run_until_disconnected()
