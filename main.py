82665186379872fb3ca0005202e32b8df506b47b
Lycif <zablotskiyalex001@gmail.com>
2025-07-20 01:43:10 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from telethon import events
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+import os
+
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+session_string = os.environ["SESSION_STRING"]
+
+client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
 @client.on(events.NewMessage)
-async def get_chat_id(event):
-    chat = await event.get_chat()
-    print(f"Chat title: {chat.title}")
-    print(f"Chat ID: {event.chat_id}")
+async def handler(event):
+    sender = await event.get_chat()
+    print(f"Название чата: {sender.title}")
+    print(f"Chat ID: {sender.id}")
+
+client.start()
+client.run_until_disconnected()
