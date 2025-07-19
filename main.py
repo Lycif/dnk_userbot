9c3ec2039165aad0749e11ccd5116a26a1bb3c51
Lycif <zablotskiyalex001@gmail.com>
2025-07-20 01:39:48 +0300

    main.py

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-from telethon import TelegramClient, events
-from telethon.sessions import StringSession
-import os
-import requests
+from telethon import events
 
-api_id = int(os.environ["API_ID"])
-api_hash = os.environ["API_HASH"]
-session_string = os.environ["SESSION_STRING"]
-bot_token = os.environ["BOT_TOKEN"]
-
-channel_ids = [-1001002714330182]
-target_chat_id = 7031349149
-
-client = TelegramClient(StringSession(session_string), api_id, api_hash)
-
-@client.on(events.NewMessage(chats=channel_ids))
-async def handler(event):
-    message_text = event.raw_text
-    requests.post(
-        f"https://api.telegram.org/bot{bot_token}/sendMessage",
-        data={"chat_id": target_chat_id, "text": message_text}
-    )
-
-client.start()
-client.run_until_disconnected()
+@client.on(events.NewMessage)
+async def get_chat_id(event):
+    chat = await event.get_chat()
+    print(f"Chat title: {chat.title}")
+    print(f"Chat ID: {event.chat_id}")
