d4157dbfb6a3460f8fd2fc8c1346dda31b4abefd
Lycif <zablotskiyalex001@gmail.com>
2025-07-19 23:48:41 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from telethon import TelegramClient
-from telethon.sessions import StringSession
+from telethon import TelegramClient, events
 import os
+from telethon.sessions import StringSession
 
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
-session_string = os.environ["SESSION_STRING"]
+session_string = os.environ["SESSION"]
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-async def main():
-    me = await client.get_me()
-    print(f"Logged in as {me.first_name} ({me.id})")
+@client.on(events.NewMessage)
+async def handler(event):
+    if event.is_private:
+        await event.reply("Привет, я живой!")
 
-with client:
-    client.loop.run_until_complete(main())
+client.start()
+client.run_until_disconnected()
