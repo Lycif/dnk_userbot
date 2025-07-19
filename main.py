57a3669bde82c059326b5c8a8c5e4d32e9dbdc6f
Lycif <zablotskiyalex001@gmail.com>
2025-07-20 02:07:29 +0300

    main.py

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ import requests
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
 session_string = os.environ["SESSION_STRING"]
-bot_token = os.environ["BOT_TOKEN"]
+bot_token = "8158303139:AAHligZP-v29MPZE3YLuUj0wwsM1Cw8lp_w"
 
-source_channel_id = -1001002714330182
-target_channel_id = -1002840952584
+source_channel_id = 1002714330182
+target_group_id = -1002840952584
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-@client.on(events.NewMessage(chats=source_channel_id))
+@client.on(events.NewMessage(chats=[source_channel_id]))
 async def handler(event):
-    message_text = event.raw_text
+    text = event.raw_text
     requests.post(
         f"https://api.telegram.org/bot{bot_token}/sendMessage",
-        data={"chat_id": target_channel_id, "text": message_text}
+        data={"chat_id": target_group_id, "text": text}
     )
 
-client.start()
-client.run_until_disconnected()
+with client:
+    client.run_until_disconnected()
