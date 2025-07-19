589918991122524b8960446bafa87882b9c05859
Lycif <zablotskiyalex001@gmail.com>
2025-07-19 23:35:44 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 import os
 
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
-session_string = os.environ["SESSION"]
+session_string = os.environ["SESSION_STRING"]
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
 async def main():
     me = await client.get_me()
-    print(f"UserBot запущен как @{me.username or me.first_name}")
+    print(f"Logged in as {me.first_name} ({me.id})")
 
 with client:
     client.loop.run_until_complete(main())
