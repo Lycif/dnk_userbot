cd6f3b7bb04d9527b6edb1d1a6d2df63fb1c2f69
Lycif <zablotskiyalex001@gmail.com>
2025-07-20 00:58:57 +0300

    main.py

--- main.py
+++ main.py
@@ -6,21 +6,20 @@ import requests
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
 session_string = os.environ["SESSION_STRING"]
+bot_token = "8158303139:AAHligZP-v29MPZE3YLuUj0wwsM1Cw8lp_w"
+bot_username = "genereytposters_bot"
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-bot_token = "8158303139:AAHligZP-v29MPZE3YLuUj0wwsM1Cw8lp_w"
-channel_id = 1002714330182
-
-@client.on(events.NewMessage(chats=[channel_id]))
+@client.on(events.NewMessage(chats=[1002714330182]))
 async def handler(event):
-    message_text = event.raw_text
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    data = {
-        "chat_id": "@genereytposters_bot",
-        "text": message_text
-    }
-    requests.post(url, data=data)
+    text = event.raw_text
+    print("Поймал сообщение:", text)
+    
+    requests.post(
+        f"https://api.telegram.org/bot{bot_token}/sendMessage",
+        data={"chat_id": "@"+bot_username, "text": text}
+    )
 
 with client:
     client.run_until_disconnected()
