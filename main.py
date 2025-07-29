419a1171df0020e6dc313c2230e547f5529eeb47
Lycif <zablotskiyalex001@gmail.com>
2025-07-29 23:57:04 +0300

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
 session_string = os.environ["SESSION_STRING"]
 
-source_channel_id = 1002714330184
+source_channel_id = 1002714330182
 target_group_id = -1002840952584
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
